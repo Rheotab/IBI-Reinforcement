49d95853fd873137bbeb6fabcbf7c597e8e3cb7c
RABUT THEO p1307400 <theo.rabut@etu.univ-lyon1.fr>
2019-12-19 14:42:50 +0100

    helloword

---
+++ IBI-Reinforcement_env/src/DQN.py
@@ -0,0 +1,18 @@
+
+class cnn(torch.nn.Module):
+    def __init__(self, D_H, D_out):
+        super(cnn, self).__init__()
+        self.conv1 = torch.nn.Conv2d(1, 10, kernel_size=5) # stripe / padding
+        self.conv2 = torch.nn.Conv2d(10, 20, kernel_size=5)
+        self.nn1 = torch.nn.Linear(320, D_H)
+        self.nn2 = torch.nn.Linear(D_H, D_out)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2)
+        x = torch.flatten(x, 1)
+        x = F.relu(self.nn1(x))
+        x = F.relu(self.nn2(x))
+        return F.log_softmax(x, dim=1)
